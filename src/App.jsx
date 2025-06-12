ee91142f58fd56420c32f2c5252bab0c041dc8d0
CHIRANTHAN610 <cchiranthan918@gmail.com>
2025-06-12 15:46:05 +0530

    use javacript spread

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
 import { Fragment } from "react";
 import Header from "./component/header";
 import Entry from "./component/Entry";
+import data from "./data";
 export default function App() {
+  const entries = data.map((entry)=>{
+    return(
+      <Entry
+      key={entry.id}
+      {...entry}
+      />
+    )
+  })
   return (
   <>
     <Header />
-    <Entry
-    img="../images/mountfunji.jpeg"
-    country="Japan"
-    googleMapLink="https://www.google.com/maps/place/%E0%B2%9C%E0%B2%AA%E0%B2%BE%E0%B2%A8%E0%B3%8D/@33.5001886,137.1538646,2872718m/data=!3m2!1e3!4b1!4m6!3m5!1s0x34674e0fd77f192f:0xf54275d47c665244!8m2!3d36.204824!4d138.252924!16zL20vMDNfM2Q?entry=ttu&g_ep=EgoyMDI1MDYwOC4wIKXMDSoASAFQAw%3D%3D"
-    title="Mount Fuji"
-    date="12 Jan,2021 -24 Jan,2021"
-    description="Mount Fuji, Japan's highest peak at 3,776 meters, is an iconic stratovolcano known for its symmetrical cone shape. It is a UNESCO World Heritage Site and a sacred symbol in Japanese culture. Popular among hikers and artists, Mount Fuji attracts millions of visitors each year."
-    />
-    <Entry
-    img="../images/fjorg.jpeg"
-    country="Iceland"
-    googleMapLink="https://www.google.com/maps/place/Iceland/@64.963051,-19.020835,6z/data=!3m1!4b1!4m6!3m5!1s0x48d6f2c7f8c8e9a5:0x7b2d8e9f8c8e9a5!8m2!3d64.963051!4d-19.020835!16zL20vMDNfM2Q?entry=ttu"
-    title="Fjorg"
-    date="1 Jan,2022 - 15 Jan,2022"
-    description="Fjorg, a stunning natural wonder in Iceland, showcases the country's unique geological features. Known for its breathtaking landscapes, including waterfalls, geysers, and volcanic formations, Fjorg attracts nature enthusiasts and photographers alike. The area is a testament to Iceland's raw beauty and geological diversity."
-    />
-    <Entry
-    img="../images/operahouse.jpeg"
-    country="Australia"
-    googleMapLink="https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2152967,15z/data=!4m6!3m5!1s0x6b12ae0f8c8e9a5:0x7b2d8e9f8c8e9a5!8m2!3d-33.8567844!4d151.2152967!16zL20vMDNfM2Q?entry=ttu"
-    title="Sydney Opera House"
-    date="20 Feb,2023 - 28 Feb,2023"
-    description="The Sydney Opera House, an architectural marvel in Australia, is renowned for its distinctive sail-like design. Located on the Sydney Harbour, it serves as a multi-venue performing arts center. The Opera House is a UNESCO World Heritage Site and a symbol of modern architecture, attracting millions of visitors each year."
-    />
+   {    entries}
   
   </>
 
---
+++ src/data.js
@@ -0,0 +1,58 @@
+export default[
+    {
+        id:1,
+        img: "../images/mountfunji.jpeg",
+        country: "Japan",
+        googleMapLink: "https://www.google.com/maps/place/%E0%B2%9C%E0%B2%AA%E0%B2%BE%E0%B2%A8%E0%B3%8D/@33.5001886,137.1538646,2872718m/data=!3m2!1e3!4b1!4m6!3m5!1s0x34674e0fd77f192f:0xf54275d47c665244!8m2!3d36.204824!4d138.252924!16zL20vMDNfM2Q?entry=ttu&g_ep=EgoyMDI1MDYwOC4wIKXMDSoASAFQAw==",    
+        title: "Mount Fuji",
+        date: "12 Jan,2021 -24 Jan,2021",
+        description: "Mount Fuji, Japan's highest peak at 3,776 meters, is an iconic stratovolcano known for its symmetrical cone shape. It is a UNESCO World Heritage Site and a sacred symbol in Japanese culture. Popular among hikers and artists, Mount Fuji attracts millions of visitors each year."
+    },
+    {
+        id:2,
+        img: "../images/fjorg.jpeg",   
+        country: "Iceland",
+        googleMapLink: "https://www.google.com/maps/place/Iceland/@64.963051,-19.020835,6z/data=!3m1!4b1!4m6!3m5!1s0x48d6f2c7f8c8e9a5:0x7b2d8e9f8c8e9a5!8m2!3d64.963051!4d-19.020835!16zL20vMDNfM2Q?entry=ttu",
+        title: "Fjorg",
+        date: "1 Jan,2022 - 15 Jan,2022",
+        description: "Fjorg, a stunning natural wonder in Iceland, showcases the country's unique geological features. Known for its breathtaking landscapes, including waterfalls, geysers, and volcanic formations, Fjorg attracts nature enthusiasts and photographers alike. The area is a testament to Iceland's raw beauty and geological diversity."
+    },
+    {
+        id:3,
+        img: "../images/operahouse.jpeg",
+        country: "Australia",
+        googleMapLink: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2152967,15z/data=!4m6!3m5!1s0x6b12ae0f8c8e9a5:0x7b2d8e9f8c8e9a5!8m2!3d-33.8567844!4d151.2152967!16zL20vMDNfM2Q?entry=ttu",
+        title: "Sydney Opera House",
+        date: "20 Feb,2023 - 28 Feb,2023",
+        description: "The Sydney Opera House, an architectural marvel in Australia, is renowned for its distinctive sail-like design. Located on the Sydney Harbour, it serves as a multi-venue performing arts center. The Opera House is a UNESCO World Heritage Site and a symbol of modern architecture, attracting millions of visitors each year."
+
+    },
+    {
+        id:4,
+        img: "../images/collesium.jpeg",        
+        country: "Italy",
+        googleMapLink: "https://www.google.com/maps/place/Colosseum/@41.8902102,12.4922316,17z/data=!3m1!4b1!4m6!3m5!1s0x132f61b7c8e9a5a5:0x7b2d8e9f8c8e9a5!8m2!3d41.8902102!4d12.4922316!16zL20vMDNfM2Q?entry=ttu",
+        title: "Colosseum",
+        date: "5 Mar,2023 - 15 Mar,2023",
+        description: "The Colosseum, an iconic symbol of ancient Rome, is a massive amphitheater that once hosted gladiatorial contests and public spectacles. Built in AD 70-80, it could hold up to 80,000 spectators. Today, it stands as a UNESCO World Heritage Site and a testament to Roman engineering and architecture."   
+
+    },
+    {
+        id:5,
+        img: "../images/greatwall.jpg",
+        country: "China",
+        googleMapLink: "https://www.google.com/maps/place/Great+Wall+of+China/@40.431908,116.570374,9z/data=!3m1!4b1!4m6!3m5!1s0x35f1a2c8e9a5a5a5:0x7b2d8e9f8c8e9a5!8m2!3d40.431908!4d116.570374!16zL20vMDNfM2Q?entry=ttu",
+        title: "Great Wall of China",
+        date: "10 Apr,2023 - 20 Apr,2023",
+        description: "The Great Wall of China, a UNESCO World Heritage Site, is a series of fortifications built to protect Chinese states from invasions. Spanning over 13,000 miles, it is one of the most impressive architectural feats in history and a symbol of China's enduring strength and resilience."
+    },
+    {
+        id:6,
+        img: "../images/tajmahal.jpeg",
+        country: "India",
+        googleMapLink: "https://www.google.com/maps/place/Taj+Mahal/@27.1750151,78.0421552,15z/data=!4m6!3m5!1s0x397478c8e9a5a5a5:0x7b2d8e9f8c8e9a5!8m2!3d27.1750151!4d78.0421552!16zL20vMDNfM2Q?entry=ttu",
+        title: "Taj Mahal",
+        date: "25 May,2023 - 5 Jun,2023",
+        description: "The Taj Mahal, a UNESCO World Heritage Site, is a stunning mausoleum built by Mughal Emperor Shah Jahan in memory of his wife Mumtaz Mahal. Known for its exquisite marble architecture and intricate carvings, it is considered one of the most beautiful buildings in the world and a symbol of eternal love."
+    }
+]
